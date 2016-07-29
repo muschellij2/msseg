d83729e52810b85f5af851e193a122a2e43fc724
muschellij2 <muschellij2@gmail.com>
2016-07-29 17:37:44 -0500

    updating with correct png_converter

--- R/png_converter.R
+++ R/png_converter.R
@@ -3,19 +3,23 @@
 #' @param pngs List of names (or with wildcards) of png files
 #' @param pdfname Output PDF filename
 #' @param extra.opts Options passed to \code{\link{im.convert}}
+#' @param interval interval of delay, passed to \code{\link{ani.options}}
+#' @param autobrowse passed to \code{\link{ani.options}}
 #' @return Name of PDF output file
 #' @export
 #' @importFrom animation ani.options im.convert
 png_converter <- function(pngs,
                           pdfname,
-                          extra.opts = "-density 300"){
+                          extra.opts = "-density 300",
+                          interval = 0,
+                          autobrowse = FALSE){
 
   ######################################
   # Turn off the animation options
   ######################################
   aniopts = ani.options()
-  ani.options(autobrowse = FALSE)
-  ani.options(interval = 0)
+  ani.options(autobrowse = autobrowse)
+  ani.options(interval = interval)
   im.convert(pngs,
              output = pdfname,
              extra.opts = extra.opts)
